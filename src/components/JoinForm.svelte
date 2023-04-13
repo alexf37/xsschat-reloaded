ee743a0119ee6a1bdc583962ac8bc382f367e161
Alex Foster <xrk4np@virginia.edu>
2023-04-13 14:49:21 -0400

    fix format

--- src/components/JoinForm.svelte
+++ src/components/JoinForm.svelte
@@ -7,16 +7,34 @@
 <svelte:head>
     <title>XSSChat</title>
 </svelte:head>
-<div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700 text-center">
-    <form class="space-y-6" action="#">
-        <h1 class="text-5xl tracking-wide font-black text-orange-500 my-8">XSSChat</h1>
-       
-            <input type="text" name="code" id="code" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="Room Code" required>
-     
-            <input type="text" name="name" id="name" placeholder="Name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white">
-    
 
-        <button type="submit" class="w-full text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:outline-none focus:ring-orange-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-orange-600 dark:hover:bg-orange-700 dark:focus:ring-orange-800">Join Room</button>
-        
+<div
+    class="w-full max-w-sm rounded-lg border border-gray-200 bg-white p-6 text-center shadow dark:border-gray-700 dark:bg-gray-800 md:p-8"
+>
+    <form class="space-y-6" action="#">
+        <h1 class="my-8 text-5xl font-black tracking-wide text-orange-500">XSSChat</h1>
+
+        <input
+            type="text"
+            name="code"
+            id="code"
+            class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-500 dark:bg-gray-600 dark:text-white dark:placeholder-gray-400"
+            placeholder="Room Code"
+            required
+        />
+
+        <input
+            type="text"
+            name="name"
+            id="name"
+            placeholder="Name"
+            class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-500 dark:bg-gray-600 dark:text-white dark:placeholder-gray-400"
+        />
+
+        <button
+            type="submit"
+            class="w-full rounded-lg bg-orange-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-orange-800 focus:outline-none focus:ring-4 focus:ring-orange-300 dark:bg-orange-600 dark:hover:bg-orange-700 dark:focus:ring-orange-800"
+            >Join Room</button
+        >
     </form>
 </div>
