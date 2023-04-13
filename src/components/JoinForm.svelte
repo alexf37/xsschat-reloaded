b9ff36cf81354e9b87b2514a17f37d2faa4d88ba
Alex Foster <122472971+alexf37@users.noreply.github.com>
2023-04-13 19:07:38 -0400

    feat: Basic Socket.IO Compatibility (#1)
    * test socket connection
    
    * bind input values
    
    * console log test
    
    * preventdefault on all join requests

--- src/components/JoinForm.svelte
+++ src/components/JoinForm.svelte
@@ -1,7 +1,15 @@
 <script lang="ts">
+    import { io } from "socket.io-client";
+    export let socket = io("https://xsschat.com");
+    let code: string;
+    let username: string;
     function submit(e: Event) {
-        console.log("test");
+        socket.emit("join", { room: code, name: username });
     }
+    socket.on('join', function (name) {
+    console.log(name);
+});
+
 </script>
 
 <svelte:head>
@@ -20,6 +28,7 @@
             id="code"
             class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-500 dark:bg-gray-600 dark:text-white dark:placeholder-gray-400"
             placeholder="Room Code"
+            bind:value={code}
             required
         />
 
@@ -28,11 +37,13 @@
             name="name"
             id="name"
             placeholder="Name"
+            bind:value={username}
             class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-500 dark:bg-gray-600 dark:text-white dark:placeholder-gray-400"
         />
 
         <button
             type="submit"
+            on:click|preventDefault={submit}
             class="w-full rounded-lg bg-orange-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-orange-800 focus:outline-none focus:ring-4 focus:ring-orange-300 dark:bg-orange-600 dark:hover:bg-orange-700 dark:focus:ring-orange-800"
             >Join Room</button
         >
