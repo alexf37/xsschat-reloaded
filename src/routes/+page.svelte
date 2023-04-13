0c0eb1c8e214afa9bca325d95022dae2d78cb52b
Alex Foster <xrk4np@virginia.edu>
2023-04-13 15:08:14 -0400

    page formatting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,27 +1,21 @@
-<script lang="ts"> 
-    import { onMount } from 'svelte';
-    import { writable } from 'svelte/store';
-    import { io } from 'socket.io-client';
-    import { browser } from '$app/environment';
-    import JoinForm from '../components/JoinForm.svelte';
-    
-    const socket = io('https://xsschat.com');
+<script lang="ts">
+    import { onMount } from "svelte";
+    import { writable } from "svelte/store";
+    import { io } from "socket.io-client";
+    import { browser } from "$app/environment";
+    import JoinForm from "../components/JoinForm.svelte";
+
+    const socket = io("https://xsschat.com");
     const messages = writable([]);
-    const message = writable('');
-    
-    onMount(async () => {
+    const message = writable("");
 
-    });
-
+    onMount(async () => {});
 </script>
-<link rel="stylesheet" href="https://rsms.me/inter/inter.css">
-<main class="flex flex-col items-center justify-center w-11/12 h-screen mx-auto">
-        <JoinForm />
+
+<svelte:head>
+    <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+</svelte:head>
+
+<main class="mx-auto flex h-screen w-11/12 flex-col items-center justify-center">
+    <JoinForm />
 </main>
-
-
-<style lang="postcss">
-    :global(html) {
-      background-color: theme(colors.gray.900);
-    }
-  </style>
