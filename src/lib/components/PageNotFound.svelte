69080a545f6cde29e8e91c89ebbde920a482b076
alexf37 <xrk4np@virginia.edu>
2023-04-14 02:39:40 -0400

    404 page

--- src/lib/components/PageNotFound.svelte
+++ src/lib/components/PageNotFound.svelte
@@ -2,17 +2,7 @@
     <title>Page Not Found</title>
 </svelte:head>
 
-<div
-    class="w-full max-w-sm rounded-lg border border-gray-200 bg-white p-6 text-center shadow dark:border-gray-700 dark:bg-gray-800 md:p-8"
->
-    <form class="space-y-6">
-        <h1 class="my-8 text-5xl font-black tracking-wide text-orange-500">404</h1>
-
-        <a
-            href="/"
-            class="w-full rounded-lg bg-orange-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-orange-800 focus:outline-none focus:ring-4 focus:ring-orange-300 dark:bg-orange-600 dark:hover:bg-orange-700 dark:focus:ring-orange-800"
-        >
-            Return to Site
-        </a>
-    </form>
+<div class="flex w-full max-w-sm flex-col items-center p-6 text-center md:p-8">
+    <h1 class="my-0 text-[10rem] font-black tracking-wide text-orange-500">404</h1>
+    <h3 class="mb-24 text-4xl font-bold tracking-wide text-white">Page not found</h3>
 </div>
