3e6fe32e5196b9769eb9ff216c851ac81126f364
alexf37 <xrk4np@virginia.edu>
2023-04-17 18:12:43 -0400

    adjust room page padding

--- src/lib/components/RoomContainer.svelte
+++ src/lib/components/RoomContainer.svelte
@@ -1,3 +1,3 @@
-<div class="mx-auto flex h-screen w-full max-w-screen-xl items-end bg-gray-900 px-4 pb-12">
+<div class="mx-auto flex h-screen w-full max-w-screen-xl items-end bg-gray-900 px-4 pb-12 pt-8">
     <slot />
 </div>
