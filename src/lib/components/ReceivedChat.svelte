6e17ed0200438ff7019a21eca04634051a2d32c7
alexf37 <xrk4np@virginia.edu>
2023-04-17 19:09:16 -0400

    create components for chat bubbles

---
+++ src/lib/components/ReceivedChat.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import UIBox from "./UIBox.svelte";
+    import type { Chat } from "$src/types/custom";
+
+    export let chat = {
+        sender: "",
+        chat: "",
+        time: "",
+    };
+</script>
+
+<UIBox class="w-9/12 max-w-fit break-words">
+    <h3>{@html chat.sender}</h3>
+    <p>{@html chat.chat}</p>
+</UIBox>
---
+++ src/lib/components/SentChat.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import UIBox from "./UIBox.svelte";
+
+    export let chat = {
+        sender: "",
+        chat: "",
+        time: "",
+    };
+</script>
+
+<UIBox class="ml-auto w-9/12 max-w-fit break-words !bg-gray-700">
+    <h3>{@html chat.sender}</h3>
+    <p>{@html chat.chat}</p>
+</UIBox>
