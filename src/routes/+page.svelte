da5804758d5e37b129377552db2ffa6890ed7d2b
alexf37 <xrk4np@virginia.edu>
2023-04-17 17:17:13 -0400

    clean up unused root page code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,7 @@
 <script lang="ts">
     import { onMount } from "svelte";
-    import { writable } from "svelte/store";
-    import { io } from "socket.io-client";
     import { browser } from "$app/environment";
     import JoinForm from "$components/JoinForm.svelte";
-
-    const socket = io("https://xsschat.com");
-    const messages = writable([]);
-    const message = writable("");
 
     onMount(async () => {});
 </script>
@@ -17,5 +11,5 @@
 </svelte:head>
 
 <main class="mx-auto flex h-screen w-11/12 flex-col items-center justify-center">
-    <JoinForm {socket} />
+    <JoinForm />
 </main>
