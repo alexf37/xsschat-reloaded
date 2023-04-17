a6ab7e2c37d5a683b3cce328ac99cdcb3f317e9a
alexf37 <xrk4np@virginia.edu>
2023-04-17 19:09:26 -0400

    implement chat bubbles on page

--- src/routes/r/[room]/+page.svelte
+++ src/routes/r/[room]/+page.svelte
@@ -1,12 +1,16 @@
 <script lang="ts">
     import RoomContainer from "$src/lib/components/RoomContainer.svelte";
     import ChatBar from "$src/lib/components/ChatBar.svelte";
+    import ReceivedChat from "$src/lib/components/ReceivedChat.svelte";
     import { usernameStore } from "$lib/stores/usernameStore";
     import { io } from "socket.io-client";
+    import SentChat from "$src/lib/components/SentChat.svelte";
+    import type { Chat } from "$src/types/custom";
 
     export let data = { room: "test" };
     let code: string = data.room;
     let username: string = $usernameStore;
+    const chats: Chat[] = [];
 
     let socket = io("https://xsschat.com");
     socket.emit("join", { room: code, name: username });
@@ -17,7 +21,11 @@
 
     socket.on("message", function (data) {
         if (data.type === "script") return;
-        console.log(`${data.name}: ${data.value}`);
+        chats.push({
+            sender: data.name,
+            chat: data.value,
+            time: new Date().toLocaleTimeString(),
+        });
     });
 
     function emit(chat: string) {
@@ -29,5 +37,12 @@
     <title>XSSChat</title>
 </svelte:head>
 <RoomContainer>
+    {#each chats as chat}
+        {#if chat.sender === username}
+            <SentChat {chat} />
+        {:else}
+            <ReceivedChat {chat} />
+        {/if}
+    {/each}
     <ChatBar {emit} />
 </RoomContainer>
