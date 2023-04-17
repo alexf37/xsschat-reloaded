30c2e52580e58f68b70a79f44a262d3044c8506c
alexf37 <xrk4np@virginia.edu>
2023-04-17 19:36:30 -0400

    format chat bubbles

--- src/lib/components/ReceivedChat.svelte
+++ src/lib/components/ReceivedChat.svelte
@@ -9,7 +9,7 @@
     };
 </script>
 
-<UIBox class="w-9/12 max-w-fit break-words">
-    <h3>{@html chat.sender}</h3>
+<UIBox class="w-9/12 max-w-fit space-y-1 break-words">
+    <h3 class="text-xs text-gray-400">{@html `${chat.sender} · ${chat.time}`}</h3>
     <p>{@html chat.chat}</p>
 </UIBox>
--- src/lib/components/SentChat.svelte
+++ src/lib/components/SentChat.svelte
@@ -8,7 +8,7 @@
     };
 </script>
 
-<UIBox class="ml-auto w-9/12 max-w-fit break-words !bg-gray-700">
-    <h3>{@html chat.sender}</h3>
+<UIBox class="ml-auto w-9/12 max-w-fit space-y-1 break-words !bg-gray-700">
+    <h3 class="text-xs text-gray-400">{@html `${chat.sender} · ${chat.time}`}</h3>
     <p>{@html chat.chat}</p>
 </UIBox>
