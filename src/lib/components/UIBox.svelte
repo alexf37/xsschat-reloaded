a61c30c6d579c5f433d43604c3a79439d75a2f79
alexf37 <xrk4np@virginia.edu>
2023-04-17 19:08:25 -0400

    default to white text in uibox

--- src/lib/components/UIBox.svelte
+++ src/lib/components/UIBox.svelte
@@ -3,6 +3,6 @@
     export { clazz as class };
 </script>
 
-<div class={"relative rounded-lg bg-gray-800 p-4 shadow-md " + clazz}>
+<div class={"relative rounded-lg bg-gray-800 p-4 text-white shadow-md " + clazz}>
     <slot />
 </div>
