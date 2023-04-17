056ea5e895dcd6690993620adce557b360d60767
alexf37 <xrk4np@virginia.edu>
2023-04-17 18:11:01 -0400

    remove unnecessary markup

--- src/lib/components/ChatBar.svelte
+++ src/lib/components/ChatBar.svelte
@@ -1,10 +1,7 @@
 <script lang="ts">
     import UIBox from "./UIBox.svelte";
-    import RoomContainer from "./RoomContainer.svelte";
 
     export let emit: (chat: string) => void;
-    export let room;
-    export let name;
 
     function submit() {
         console.log("submit: " + chat);
@@ -15,39 +12,21 @@
     let chat = "";
 </script>
 
-<RoomContainer>
-    <UIBox>
-        <form
-            class="flex flex-row items-center justify-between space-x-4 space-y-0"
-            on:submit={submit}
+<UIBox class="w-full">
+    <form class="flex flex-row items-center justify-between gap-4" on:submit={submit}>
+        <label for="chat-box" class="sr-only">Chat Input</label>
+        <input
+            type="text"
+            id="chat-box"
+            bind:value={chat}
+            class="w-full flex-1 rounded-lg border border-gray-600 bg-gray-700 px-3 py-2 text-sm text-white placeholder-gray-400 focus:border-gray-500 focus:ring-gray-400"
+            placeholder="XSSChat"
+        />
+        <button
+            type="submit"
+            class="flex items-center justify-center rounded-lg bg-orange-600 px-4 py-2 text-sm font-medium text-white hover:bg-orange-500 focus:outline-none focus:ring focus:ring-orange-700"
         >
-            <div class="w-full">
-                <div class="flex items-center">
-                    <label for="chat-box" class="sr-only">Chat Input</label>
-                    <div class="relative w-full">
-                        <div
-                            class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3"
-                        />
-                        <input
-                            type="text"
-                            id="chat-box"
-                            bind:value={chat}
-                            class="w-full rounded-lg border border-gray-600 bg-gray-700 px-3 py-2 text-sm text-white placeholder-gray-400 focus:border-gray-500 focus:ring-gray-400"
-                            placeholder="XSSChat"
-                        />
-                    </div>
-                </div>
-            </div>
-            <div
-                class="flex flex-shrink-0 flex-col items-stretch justify-end space-y-2 md:w-auto md:flex-row md:items-center md:space-x-3 md:space-y-0"
-            >
-                <button
-                    type="submit"
-                    class="flex items-center justify-center rounded-lg bg-orange-600 px-4 py-2 text-sm font-medium text-white hover:bg-orange-500 focus:outline-none focus:ring focus:ring-orange-700"
-                >
-                    Send
-                </button>
-            </div>
-        </form>
-    </UIBox>
-</RoomContainer>
+            Send
+        </button>
+    </form>
+</UIBox>
