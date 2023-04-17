77e10f84316ec9359dd2eae38aeb2805ad059023
alexf37 <xrk4np@virginia.edu>
2023-04-17 18:11:32 -0400

    add support for additional UIBox classes

--- src/lib/components/UIBox.svelte
+++ src/lib/components/UIBox.svelte
@@ -1,3 +1,8 @@
-<div class="relative rounded-lg bg-gray-800 p-4 shadow-md">
+<script lang="ts">
+    let clazz = "";
+    export { clazz as class };
+</script>
+
+<div class={"relative rounded-lg bg-gray-800 p-4 shadow-md " + clazz}>
     <slot />
 </div>
