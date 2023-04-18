f0bfcc7992f27273d6b90de4d0586d5df6d9ac0e
alexf37 <xrk4np@virginia.edu>
2023-04-17 20:53:26 -0400

    alight sent chats right

--- src/lib/components/SentChat.svelte
+++ src/lib/components/SentChat.svelte
@@ -8,7 +8,7 @@
     };
 </script>
 
-<UIBox class="ml-auto w-9/12 max-w-fit space-y-1 break-words !bg-gray-700">
+<UIBox class="ml-auto w-9/12 max-w-fit space-y-1 break-words !bg-gray-700 text-right">
     <h3 class="text-xs text-gray-400">{@html `${chat.sender} · ${chat.time}`}</h3>
     <p>{@html chat.chat}</p>
 </UIBox>
