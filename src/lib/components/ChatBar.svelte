aaa031a1e21fc38a09d5318cdececd1029743f31
alexf37 <xrk4np@virginia.edu>
2023-04-18 01:59:50 -0400

    port over old html autocomplete

--- src/lib/components/ChatBar.svelte
+++ src/lib/components/ChatBar.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import { onMount } from "svelte";
+    import { onMount, tick } from "svelte";
     import UIBox from "./UIBox.svelte";
 
     export let emit: (chat: string) => void;
@@ -8,7 +8,41 @@
 
     onMount(() => {
         console.log("chatbar mounted");
-        chatbar.focus();
+
+        chatbar.addEventListener("keydown", async function (e) {
+            if (e.code == "Tab") {
+                e.preventDefault();
+                let message = chat;
+                let cursorPosition = chatbar.selectionStart ?? 0;
+                let elemName =
+                    (chat.substring(0, cursorPosition).split(" ") ?? chat)
+                        .pop()
+                        ?.split(">")
+                        .pop() ?? chat;
+                let insertIndex = cursorPosition - elemName.length;
+                try {
+                    if (
+                        document.createElement(elemName.toLowerCase()).toString() ==
+                        "[object HTMLUnknownElement]"
+                    ) {
+                        return;
+                    }
+                } catch (e) {
+                    console.log(e);
+                    chatbar.focus();
+                    return;
+                }
+                let elem = `<${elemName}></${elemName}>`;
+                let newInput = message.slice(0, insertIndex) + elem + message.slice(cursorPosition);
+                chat = newInput;
+                console.log(cursorPosition);
+                cursorPosition =
+                    (message.slice(0, insertIndex) + elem).length - `</${elemName}>`.length;
+                await tick();
+                chatbar.setSelectionRange(cursorPosition, cursorPosition, "backward");
+                console.log(cursorPosition);
+            }
+        });
     });
 
     function submit() {
@@ -16,13 +50,13 @@
         console.log("submit: " + chat);
         emit(chat);
         chat = "";
-        chatbar.focus();
     }
 </script>
 
 <UIBox class="z-50 mt-auto w-full min-w-fit">
     <form class="flex flex-row items-center justify-between gap-4" on:submit={submit}>
         <label for="chat-box" class="sr-only">Chat Input</label>
+        <!-- svelte-ignore a11y-autofocus -->
         <input
             type="text"
             id="chat-box"
@@ -30,6 +64,7 @@
             bind:this={chatbar}
             class="w-full flex-1 rounded-lg border border-gray-600 bg-gray-700 px-3 py-2 text-sm text-white placeholder-gray-400 focus:border-gray-500 focus:ring-gray-400"
             placeholder="XSSChat"
+            autofocus
         />
         <button
             type="submit"
