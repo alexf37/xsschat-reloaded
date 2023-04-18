25e9ef3b990b12c52fe69e249afdd183c0be88d4
alexf37 <xrk4np@virginia.edu>
2023-04-18 01:10:37 -0400

    eliminate useless styles

--- src/lib/components/RoomContainer.svelte
+++ src/lib/components/RoomContainer.svelte
@@ -1,5 +1,3 @@
-<div
-    class="mx-auto flex h-full w-full max-w-screen-xl flex-col overflow-hidden bg-gray-900 px-4 pb-4 pt-8 md:pb-8"
->
+<div class="mx-auto flex h-full max-w-screen-xl flex-col bg-gray-900 px-4 pb-4 pt-8 md:pb-8">
     <slot />
 </div>
