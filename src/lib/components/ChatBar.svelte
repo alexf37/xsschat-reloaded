9f70214c3067bf18971236cddace234af2ab66bc
alexf37 <xrk4np@virginia.edu>
2023-04-17 22:15:48 -0400

    fix chatbar overflow

--- src/lib/components/ChatBar.svelte
+++ src/lib/components/ChatBar.svelte
@@ -19,7 +19,7 @@
     }
 </script>
 
-<UIBox class="z-50 mt-auto w-full">
+<UIBox class="z-50 mt-auto w-full min-w-fit">
     <form class="flex flex-row items-center justify-between gap-4" on:submit={submit}>
         <label for="chat-box" class="sr-only">Chat Input</label>
         <input
