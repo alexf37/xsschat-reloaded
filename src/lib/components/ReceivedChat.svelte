d06f581d6226b95240af857b79c9a3b5b3682c4c
alexf37 <xrk4np@virginia.edu>
2023-04-22 17:06:55 -0400

    add verified badge on server event msgs

--- src/lib/components/ReceivedChat.svelte
+++ src/lib/components/ReceivedChat.svelte
@@ -9,6 +9,8 @@
 </script>
 
 <UIBox class="w-9/12 max-w-fit space-y-1 break-words">
-    <h3 class="text-xs text-gray-400">{@html `${chat.sender} · ${chat.time}`}</h3>
+    <h3 class="flex flex-row items-center gap-1 text-xs text-gray-400">
+        {@html `${chat.sender} · ${chat.time}`}
+    </h3>
     <p>{@html chat.chat}</p>
 </UIBox>
