47b36770d42626c9ec180dcb88f0518cd04147f9
alexf37 <xrk4np@virginia.edu>
2023-04-22 16:29:34 -0400

    add viewport sizing for PWA

--- src/lib/components/RoomContainer.svelte
+++ src/lib/components/RoomContainer.svelte
@@ -1,3 +1,5 @@
-<div class="mx-auto flex h-full max-w-screen-xl flex-col bg-gray-900 px-4 pb-4 pt-8 md:pb-8">
+<div
+    class="mx-auto flex h-full max-w-screen-xl flex-col bg-gray-900 px-4 pb-[max(1rem,_calc(1rem_+_env(safe-area-inset-bottom)))] pt-8 md:pb-8"
+>
     <slot />
 </div>
